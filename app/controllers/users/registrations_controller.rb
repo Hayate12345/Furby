0d3af643752ca7ab44b704e9b7bac670b9378454
Hayate_T <hayatesato1@gmail.com>
2023-02-25 02:01:21 +0900

    ユーザ認証機能、メール認証完成

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -41,7 +41,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   # ストロングパラメータの追加
-  def configure_sign_up_parameters
+  def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
