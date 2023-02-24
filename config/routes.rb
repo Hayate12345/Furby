25be8a752109a0326c1ff2a8703cd8dc25a17ef2
Hayate_T <hayatesato1@gmail.com>
2023-02-25 02:37:33 +0900

    愛護団体モデルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  devise_for :welfares
   devise_for :users, controllers: { registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords", confirmations: "users/confirmations", unlocks: "users/unlocks" }
 end
