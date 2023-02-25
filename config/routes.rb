02f6427bb6c5e6f53a6ed622ec798cc7f8e64b05
Hayate_T <hayatesato1@gmail.com>
2023-02-25 09:43:17 +0900

    topページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   devise_for :welfares, controllers: { registrations: "welfares/registrations", sessions: "welfares/sessions", passwords: "welfares/passwords", confirmations: "welfares/confirmations", unlocks: "welfares/unlocks" }
 
+  root to: "pages/tops#index"
+
   # 動物愛護団体関連
   get "welfares/posts/new", to: "welfares/posts/posts#new"
 end
