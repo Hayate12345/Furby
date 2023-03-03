18f6822dfd17e88215c2862df7c89fc414af5205
Hayate_T <hayatesato1@gmail.com>
2023-03-03 19:03:33 +0900

    jqueryの動作確認

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,13 @@ import * as ActiveStorage from '@rails/activestorage';
 import 'channels';
 import 'bootstrap';
 import '../stylesheets/application.scss';
+require('jquery');
+import './welfares/posts';
+
+// * フラッシュメッセージを4秒後に消す
+$(function() {
+    setTimeout("$('.alert').fadeOut('slow')", 2000);
+});
 
 Rails.start();
 Turbolinks.start();
---
+++ app/javascript/packs/welfares/posts.js
@@ -0,0 +1,4 @@
+// * フラッシュメッセージを4秒後に消す
+$(function() {
+    setTimeout("$('.alert').fadeOut('slow')", 2000);
+});
